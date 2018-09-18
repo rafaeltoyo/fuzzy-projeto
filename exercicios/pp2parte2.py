9cb18f3bc993ca54014d9481d3f8c7a2cc656fb0
RAFAEL HIDEO TOYOMOTO <rafael_toyo@hotmail.com>
2018-09-18 14:37:06 -0300

    Enunciado ex2.2

--- exercicios/pp2parte2.py
+++ exercicios/pp2parte2.py
@@ -38,6 +38,18 @@ def func3():
 
 
 def main():
+    """
+    Funcao Main
+    :return: None
+    """
+    
+    """
+    1) Regras
+        1) Se eh alto e pesado entao eh forte
+        2) Se tem altura media e tem peso moderado entao eh levemente forte
+        3) Se eh baixo e leve entao tem forca media
+    """
+
     altura = func1()
     altura.plot()
     plt.show()
@@ -50,6 +62,51 @@ def main():
     forca.plot()
     plt.show()
 
+    """
+    1.1) Calcular e plotar os antecedentes da regra:
+        1) alto e pesado
+        2) altura media e peso moderado
+        3) baixo e leve
+    """
+    pass
+
+    """
+    1.2) Obter cada relacao que descreve as regras Rj (j=1,2,3).
+        Operador agregacao E -> min ou prod (usuario escolhe)
+    """
+    pass
+
+    """
+    2) 
+    input = levemente alto e muito pesado
+        Conc1: input * R1
+        Conc2: input * R2
+        Conc3: input * R3
+        onde * -> operador de composicao sup-min
+        Conc = Uniao(Conc1, Conc2, Conc3)
+        
+        Dicas:
+        2.1) input -> matriz bidimensional
+        2.2) extensao cilindrica do input -> matriz tridimensional
+        2.3) Relacao Rj de cada regra -> matriz tridimensional
+        2.4) Conc_j = Projecao em X1 e X2 (altura e peso)
+        2.5) Conc = Uniao(Conc_j, j{1,2,3})
+    """
+    pass
+
+    """
+    3) parecido com o 2
+    Conc: input * R
+    onde R = Uniao(R1, R2, R3)
+    
+        Dicas:
+        3.1) input -> matriz bidimensional
+        3.2) extensao cilindrica do input -> matriz tridimensional
+        3.3) Relacao Rj de cada regra -> matriz tridimensional
+        3.4) R=Uniao(R_j, j={1,2,3})
+        3.5) Conc = Projecao em X1 e X2 (altura e peso)
+    """
+    pass
 
 if __name__ == "__main__":
     main()
